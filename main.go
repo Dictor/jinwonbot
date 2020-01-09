2a760dbcf954100905657d1f0156ceec21a1ad60
Dictor <kimdictor@gmail.com>
2020-01-09 14:43:48 +0000

    Apply new seperate log function and Add comments

--- main.go
+++ main.go
@@ -4,13 +4,27 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"log"
 	"time"
 )
 
 var currentDoorStatus bool
 var latestChangeTime int64
 
+func checkError(explain string, err error) {
+	if err != nil {
+		log.Panicf("[%s → Fatal error!] %v", explain, err)
+	}
+}
+
 func main() {
+	/* Set logging */
+	log_path, _ := setLogPath() // Ignore error in this line is finally cause panic in set log stream anyway.
+	fp, err := setLogStream(log_path)
+	defer fp.Close()
+	checkError("Set logging", err)
+
+	/* Get CLI flags */
 	var bot_token, web_path string
 	var insert_period int
 	flag.StringVar(&bot_token, "t", "", "Bot's Token string")
@@ -18,18 +32,14 @@ func main() {
 	flag.IntVar(&insert_period, "d", 60, "Getting web information task's period (second)")
 	flag.Parse()
 
-	err := openDB("./db.db")
-	if err != nil {
-		fmt.Println("[DB Open Error]", err)
-		return
-	}
+	/* Open DB */
+	err = openDB("./db.db")
+	checkError("DB Open", err)
 	fmt.Println("[DB opened successfully]")
 
+	/* Start discord bot */
 	err = startBot(bot_token)
-	if err != nil {
-		fmt.Println("[Discord Bot Error]", err)
-		return
-	}
+	checkError("Starting bot", err)
 	fmt.Println("[Bot started successfully]")
 
 	chan_change_status := make(chan bool)
@@ -40,21 +50,21 @@ func main() {
 		for {
 			res_html, err := getRawHtml(web_path)
 			if err != nil {
-				fmt.Println("[HTTP Error]", err)
+				log.Println("[HTTP Error]", err)
 			} else {
 				now_status := DoorStatus{time.Now().Unix(), isDoorOpen(res_html)}
 				err = insertStatus(&now_status)
 				if err != nil {
-					fmt.Println("[DB Insert Error]", err)
+					log.Println("[DB Insert Error]", err)
 				} else {
-					fmt.Print("i")
+					log.Print("i")
 				}
 			}
 
 			res_last, err := getLatestStatus()
 			if err != nil {
 				if err != sql.ErrNoRows {
-					fmt.Println("[DB Select Error]", err)
+					log.Println("[DB Select Error]", err)
 				}
 			} else {
 				if res_last.Status != last_status {
@@ -66,7 +76,7 @@ func main() {
 			res_time, err := getLatestConditionStatus(!last_status)
 			if err != nil {
 				if err != sql.ErrNoRows {
-					fmt.Println("[DB Select Error]", err)
+					log.Println("[DB Select Error]", err)
 				}
 			} else {
 				chan_time <- res_time.Time
@@ -79,7 +89,7 @@ func main() {
 	for {
 		select {
 		case currentDoorStatus = <-chan_change_status:
-			fmt.Println("[Status changed] →", currentDoorStatus)
+			log.Println("[Status changed] →", currentDoorStatus)
 		case latestChangeTime = <-chan_change_time:
 		}
 	}
