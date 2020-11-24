947b316c6e487c1cdaa6ad621d5706cd60823a99
김정현 <kimdictor@gmail.com>
2020-11-24 19:59:57 +0900

    Fix array addressing mistype

--- discord.go
+++ discord.go
@@ -31,7 +31,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	} else {
 		pContent := strings.Split(m.Content, " ")
 		switch len(pContent) {
-		case 0:
+		case 1:
 			if strings.Contains(m.Content, "진원쿤") && utf8.RuneCountInString(m.Content) <= 5 {
 				var now_answer string = "지금 바라미실은 "
 				var time_string string
@@ -53,9 +53,9 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				log.Printf("[Message Send](%s-%s): %s → %s \n", m.Author.ID, m.Author.Username, m.Content, now_answer)
 				s.ChannelMessageSend(m.ChannelID, now_answer)
 			}
-		case 1:
+		case 2:
 			if strings.Contains(pContent[1], "정보") {
-				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("저는 진원봇 %s 입니다!\n저에 대해선 https://github.com/Dictor/jinwonbot에서 자세히 알아보실수 있어요!\n 참고로 저는 (현실)진원쿤이 만들어준 사이트에서 정보를 끌고온답니다.\n 마찬가지로 https://github.com/ibarami/IsBaramiOpen에서 확인하실 수 있어요!", version))
+				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("저는 진원봇 %s 입니다!\n저에 대해선 https://github.com/Dictor/jinwonbot 에서 자세히 알아보실수 있어요!\n참고로 저는 (현실)진원쿤이 만들어준 사이트( https://github.com/ibarami/IsBaramiOpen )에서 정보를 끌고온답니다!!", version))
 			}
 		}
 	}
