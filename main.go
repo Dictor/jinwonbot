59f94db4778310e825dad0ca35874be65e935e6c
김정현 <kimdictor@gmail.com>
2020-11-24 20:00:21 +0900

    Change data retrieving method to git

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
-	"fmt"
 	"log"
 	"time"
 
@@ -35,7 +33,7 @@ func main() {
 	flag.Parse()
 
 	/* Open DB */
-	err := openDB("./db.db")
+	isNewStore, err := OpenStore("./db.db")
 	checkError("DB Open", err)
 	log.Println("[DB opened successfully]")
 
@@ -44,43 +42,59 @@ func main() {
 	checkError("Starting bot", err)
 	log.Println("[Bot started successfully]")
 
+	/* Get github information */
+	repo, err := CloneGitRepository("https://github.com/ibarami/ibarami.github.io")
+	checkError("Clone repo", err)
+	if isNewStore {
+		commits, err := ListRepositoryCommits(repo, time.Time{})
+		checkError("Get commits", err)
+		checkError("Update DB", AppendStore(commits...))
+		checkError("Save DB", SaveStore())
+	}
+
 	chan_change_status := make(chan bool)
 	chan_change_time := make(chan int64)
 
 	go func(chan_status chan bool, chan_time chan int64) {
 		var last_status bool
 		for {
-			res_html, err := getRawHtml(web_path)
+
+			commit, err := SelectLatestCommit()
 			if err != nil {
-				log.Println("[HTTP Error]", err)
-			} else {
-				now_status := DoorStatus{time.Now().Unix(), isDoorOpen(res_html)}
-				err = insertStatus(&now_status)
+				log.Println("[Commit retrieve error]", err)
+			}
+			repo, err = CloneGitRepository("https://github.com/ibarami/ibarami.github.io")
+			if err != nil {
+				log.Println("[repo clone error]", err)
+			}
+			commits, err := ListRepositoryCommits(repo, commit.CommitTime)
+			if err != nil {
+				log.Println("[Commit select error]", err)
+			}
+			if len(commits) > 0 {
+				err := AppendStore(commits...)
 				if err != nil {
-					log.Println("[DB Insert Error]", err)
-				} else {
-					fmt.Print("i")
+					log.Println("[append store error]", err)
+				}
+				err = SaveStore()
+				if err != nil {
+					log.Println("[save store error]", err)
 				}
 			}
 
-			res_last, err := getLatestStatus()
 			if err != nil {
-				if err != sql.ErrNoRows {
-					log.Println("[DB Select Error]", err)
-				}
-			} else {
-				if res_last.Status != last_status {
-					last_status = res_last.Status
-					chan_status <- res_last.Status
-				}
+				log.Println("[DB Select Error]", err)
+			}
+			if commit.IsOpen != last_status {
+				last_status = commit.IsOpen
+				chan_status <- commit.IsOpen
 			}
 
-			res_time, err := getLatestConditionStatus(!last_status)
-			if err != nil && err != sql.ErrNoRows {
-				log.Printf("[DB Select Error] %s\n", err)
-			} else {
-				chan_time <- res_time.Time
+			scommit, err := SelectLatestStatus(last_status)
+			if err != nil {
+				log.Println("[DB Select Error]", err)
 			}
+			chan_time <- scommit.CommitTime.Unix()
 
 			time.Sleep(time.Duration(insert_period) * time.Second)
 		}
