ab9bf1e850baf6381c08d6ccadeffbceae05e0b8
김정현 <kimdictor@gmail.com>
2020-12-03 15:42:36 +0900

    change default logger to logrus

--- go.mod
+++ go.mod
@@ -4,16 +4,16 @@ go 1.13
 
 require (
 	github.com/bwmarrin/discordgo v0.22.0
+	github.com/dictor/echologrus v1.1.5
 	github.com/dictor/justlog v1.1.1
 	github.com/go-git/go-billy/v5 v5.0.0
 	github.com/go-git/go-git/v5 v5.2.0
 	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/labstack/echo/v4 v4.1.17
 	github.com/mattn/go-sqlite3 v2.0.2+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/samonzeweb/godb v1.0.13
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
-	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,60 +5,77 @@ import (
 	"log"
 	"time"
 
-	"github.com/dictor/justlog"
+	elogrus "github.com/dictor/echologrus"
+	"github.com/go-git/go-git/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 )
 
 var (
 	gitTag, gitHash, buildDate string // build flags
 	currentDoorStatus          bool   // latest door status
 	latestChangeTime           int64  // latest door status changed timestamp
+	// GlobalLogger is global default logger in whole program
+	GlobalLogger elogrus.EchoLogger
 )
 
-func checkError(explain string, err error) {
+func mustAction(action string, err error) {
 	if err != nil {
-		log.Panicf("[%s → Fatal error!] %v", explain, err)
+		GlobalLogger.WithFields(logrus.Fields{
+			"action": action,
+			"err":    err,
+		}).Panic("necessary action failed")
 	}
 }
 
 func main() {
-	/* Set logging */
-	logPath := justlog.MustPath(justlog.SetPath())
-	defer (justlog.MustStream(justlog.SetStream(logPath))).Close()
-	log.Printf("jinwonbot %s (%s) - %s", gitTag, gitHash, buildDate)
+	/* Init webserver and logger */
+	e := echo.New()
+	GlobalLogger = elogrus.Attach(e)
+	GlobalLogger.Infof("jinwonbot %s (%s) - %s", gitTag, gitHash, buildDate)
 
 	/* Get CLI flags */
 	var (
-		botToken, repoPath string
-		insertPeriod       int
+		botToken, repoPath, storePath, listenPath string
+		insertPeriod                              int
 	)
 	flag.StringVar(&botToken, "t", "", "Bot's Token string")
-	flag.StringVar(&repoPath, "r", "https://github.com/ibarami/ibarami.github.io", "Repository path for check door status")
+	flag.StringVar(&repoPath, "repo", "https://github.com/ibarami/ibarami.github.io", "Repository path for check door status")
 	flag.IntVar(&insertPeriod, "d", 60, "Getting web information task's period (second)")
+	flag.StringVar(&storePath, "store", "./db.db", "Commit store file's path")
+	flag.StringVar(&listenPath, "listen", ":80", "Listen address for web server")
 	flag.Parse()
 
 	/* Open DB */
-	isNewStore, err := OpenStore("./db.db")
-	checkError("store open", err)
-	log.Println("commit store opened successfully!")
+	isNewStore, err := OpenStore(storePath)
+	mustAction("store open", err)
+	GlobalLogger.Infoln("commit store opened successfully!")
 
 	/* Start discord bot */
 	err = startBot(botToken)
-	checkError("starting bot", err)
-	log.Println("discord bot started successfully!")
+	mustAction("start bot", err)
+	GlobalLogger.Infoln("discord bot started successfully!")
 
 	/* Get github information */
 	repo, err := CloneGitRepository(repoPath)
-	checkError("clone repo", err)
+	mustAction("clone repo", err)
 	if isNewStore {
-		log.Println("there isn't saved commit store. creating...")
+		GlobalLogger.Infoln("there isn't saved commit store. creating...")
 		commits, err := ListRepositoryCommits(repo, time.Time{})
-		checkError("get commits", err)
-		log.Printf("%d commits retieved!", len(commits))
-		checkError("append store", AppendStore(commits...))
-		checkError("save store", SaveStore())
+		mustAction("get commits", err)
+		GlobalLogger.Infof("%d commits retieved!", len(commits))
+		mustAction("append commits", AppendStore(commits...))
+		mustAction("save store", SaveStore())
 	}
-	log.Println("github repo cloned successfully!")
+	GlobalLogger.Infof("github repo cloned successfully!")
+	go UpdateStatusLoop(repo, insertPeriod)
 
+	/* Start web server */
+	e.Logger.Fatal(e.Start(listenPath))
+}
+
+// UpdateStatusLoop is update door status in infinity loop
+func UpdateStatusLoop(repo *git.Repository, delayPeriod int) {
 	for {
 		lcommit, err := SelectLatestCommit()
 		if err != nil {
@@ -84,6 +101,6 @@ func main() {
 			}
 		}
 
-		time.Sleep(time.Second * time.Duration(insertPeriod))
+		time.Sleep(time.Second * time.Duration(delayPeriod))
 	}
 }
